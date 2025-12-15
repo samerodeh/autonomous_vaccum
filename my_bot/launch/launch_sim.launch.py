eece8e0c7c0ae624f62d8a4a6b88ef89d83b7246
Your Name <your.email@example.com>
2025-12-15 04:48:59 +0000

    updated main

--- my_bot/launch/launch_sim.launch.py
+++ my_bot/launch/launch_sim.launch.py
@@ -1,17 +1,14 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
-
 def generate_launch_description():
-
     package_name = 'my_bot'
 
-    # Robot State Publisher (loads your URDF)
+    # Robot State Publisher
     rsp = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
             os.path.join(get_package_share_directory(package_name), 'launch', 'rsp.launch.py')
@@ -19,18 +16,23 @@ def generate_launch_description():
         launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-    # Gazebo (no params file)
+    # Gazebo
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
             os.path.join(
-                get_package_share_directory('gazebo_ros'), 
-                'launch', 
+                get_package_share_directory('gazebo_ros'),
+                'launch',
                 'gazebo.launch.py'
             )
-        ])
+        ]),
+        launch_arguments={
+            'verbose': 'false',
+            'gui': 'true',
+            'server': 'true'
+        }.items()
     )
 
-    # Spawn robot in Gazebo
+    # Spawn robot
     spawn = Node(
         package='gazebo_ros',
         executable='spawn_entity.py',
@@ -41,6 +43,5 @@ def generate_launch_description():
     return LaunchDescription([
         rsp,
         gazebo,
-        spawn
+        spawn,
     ])
-
